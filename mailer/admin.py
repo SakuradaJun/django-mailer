3b191bcab39170ee1971aa9d52a16253321d9689
Yuji Tomita Macbook <yuchant@gmail.com>
2011-04-12 20:42:07 -0300

    Decode email message body on detail view.

--- mailer/admin.py
+++ mailer/admin.py
@@ -5,7 +5,16 @@ from mailer.models import Message, DontSendEntry, MessageLog
 
 class MessageAdmin(admin.ModelAdmin):
     list_display = ["id", "account", "to_addresses", "subject", "when_added", "priority"]
-
+    
+    readonly_fields = ['subject', 'to_addresses', 'body']
+    
+    def body(self, obj):
+        email = obj.email
+        if email is not None:
+            return email.body.replace('\n', '<br>')
+        return ""
+    body.allow_tags = True
+    
 
 class DontSendEntryAdmin(admin.ModelAdmin):
     list_display = ["to_address", "when_added"]
@@ -13,7 +22,14 @@ class DontSendEntryAdmin(admin.ModelAdmin):
 
 class MessageLogAdmin(admin.ModelAdmin):
     list_display = ["id", "account", "to_addresses", "subject", "when_attempted", "result"]
-
+    readonly_fields = ['subject', 'to_addresses', 'body']
+    
+    def body(self, obj):
+        email = obj.email
+        if email is not None:
+            return email.body.replace('\n', '<br>')
+        return ""
+    body.allow_tags = True
 
 admin.site.register(Message, MessageAdmin)
 admin.site.register(DontSendEntry, DontSendEntryAdmin)
